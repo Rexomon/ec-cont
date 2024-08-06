6e71dabba999d0176bb36f893594c8f3a4f1997e
Rexomon <muhammadindriyo12@gmail.com>
2024-08-06 15:41:18 +0700

    #

--- ctc-bk/src/index.ts
+++ ctc-bk/src/index.ts
@@ -14,11 +14,7 @@ const corsOptions = {
 	origin: "http://localhost:4518",
 	credentials: true,
 };
-
-app.get("/wf", (req, res) => {
-    res.sendFile(`${__dirname}/wf.html`);
-});
-
+~
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
